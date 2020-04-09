6a7644b1477ee602b915a6b18c2eb68ad7089fb6
Mirjam <klammsteinermirjam@gmail.com>
2020-04-09 19:01:16 +0200

    Able to write score to db

--- game/script.js
+++ game/script.js
@@ -1,220 +1,182 @@
-$(document).ready(function(){
-    var canvas = document.getElementById("game");
-    var context = canvas.getContext("2d");
-    var timer = document.getElementById("timer");
+const settings = {
+  game: document.getElementById("game"),
+  moles: 9,
+  // difficulty 1-3; 1 easy, 2 medium, 3 hard
+  difficulty: 2,
+  counter: 0,
+};
 
-    // damit die Bilder nicht gestaucht/gestreckt werden, aber trotzdem die Größe des Canvas geändert wird
-    $(canvas)
-    .prop("width", window.innerWidth-200)
-    .prop("height", window.innerHeight-200);
-
-    // Darstellungseinstellungen
-    context.webkitImageSmoothingEnabled = false;
-    context.mozImageSmoothingEnabled = false;
-    context.imageSmoothingEnabled = false;
-    context.imageSmoothingQuality = "high";
-
-    // für Platzberechnung benötigte Variablen
-    let canvasWidth = window.innerWidth-200;
-    let canvasHeight = window.innerHeight-200;
-    // Wie viele Maulwürfe, will man haben
-    let amount = 6;
-    let rows = 2;
-    let rowDifference = 300;
-    let startPosLeft = 0;
-    let startHeight = 0;
-
-    // Mole Werte
-    let mWidth = 200;
-    let mHeight;
-    let abstand = (canvasWidth - (mWidth*(amount/rows)))/((amount/rows)+1);
-
-    // Tunnel Werte
-    let tWidth = 250;
-    let tHeight;
-    let tAbstand = (canvasWidth - (tWidth*(amount/rows)))/((amount/rows)+1);
-
-    // Sprites
-    var sprite = new Image();
-    var tunnel = new Image();
-
-    // setzen der Sprites
-    sprite.src = "../images/mole.svg"
- 
-    tunnel.loaded = false;
-    tunnel.onload = function() {
-        this.loaded = true;
-        drawTunnel();
+$(document).ready(function () {
+  // Wenn Button mit Schwierigkeit gedrückt wird, dann wird das Spiel gestartet
+  $(".difficultyButton").click(function () {
+    let buttonDiv = document.getElementById("buttons");
+    settings.difficulty = parseInt($(this).val(), 10);
+    for (let i = 0; i < $("button").length; i++) {
+      $("button")[i].style.display = "none";
     }
-    tunnel.src = "../images/tunnel.svg"
-    
+    init();
 
-    // Timer
+    buttonDiv.style.display = "none";
+  });
+});
+
+function init() {
+  let timer = document.getElementById("timer");
+  let speed;
+  switch (settings.difficulty) {
+    case 1:
+      speed = 3;
+      break;
+    case 2:
+      speed = 2;
+      break;
+    case 3:
+      speed = 1.2;
+      break;
+    case 999:
+      speed = 0.3;
+      break;
+  }
+
+  for (let index = 0; index < settings.moles; index++) {
+    // Div für Mole
+    let mole = document.createElement("div");
+    mole.id = "mole" + index.toString();
+    mole.className = "grid-item";
+
+    // "Dirt", Bereich aus dem der Mole herauskommt
+    let imageDirt = document.createElement("div");
+    imageDirt.classList = "dirt";
+
+    // Maulwurf
+    let moleImage = document.createElement("img");
+    moleImage.src = "../images/firstMole.svg";
+    moleImage.classList = "mole";
+
+    let spawnInterval = setInterval(() => {
+      colorSetter(moleImage);
+      $("#game").removeClass("noshow");
+
+      if (Math.random() > 0.97) {
+        // damit nur getoggelt wird, wenn der Mole noch nicht herausen ist
+        if (!moleImage.classList.contains("alive")) {
+          moleImage.classList.toggle("alive");
+        }
+      }
+
+      for (let i = 0; i < $(".alive").length; i++) {
+        $(".alive")[i].style.animationDuration = speed.toString() + "s";
+      }
+    }, 100); //update interval
+
+    // remove alive after animation ends
+    $(mole).on("animationend webkitAnimationEnd", function () {
+      if (moleImage.classList.contains("alive")) {
+        moleImage.classList.toggle("alive");
+      }
+      if (moleImage.classList.contains("normalMole")) {
+        moleImage.classList.remove("normalMole");
+      } else if (moleImage.classList.contains("redMole")) {
+        moleImage.classList.remove("redMole");
+      } else if (moleImage.classList.contains("yellowMole")) {
+        moleImage.classList.remove("yellowMole");
+      }
+    });
+
+    // TIMER
     let time = 10; // in Sekunden
     let timeID; // zum Stoppen, wenn Zeit abgelaufen
     function drawElapsedTime() {
-        if(time < 0) {
-            clearInterval(timeID);
-        } else {
-            $(timer).text(time + " secs");
-            time--;   
+      if (time < 0) {
+        // countdown stoppen und spawnen von Maulwürfen stoppen
+        clearInterval(timeID);
+        clearInterval(spawnInterval);
+
+        for (let i = 0; i < settings.moles; i++) {
+          moleImage.classList.remove("alive");
         }
+        // Endbutton erscheinen lassen
+        let endArea = document.getElementById("endArea");
+        endArea.style.display = "block";
+        let endButton = document.getElementById("endButton");
+        endButton.style.display = "block";
+      } else {
+        $(timer).text(time + " secs");
+        time--;
+      }
     }
+    timeID = setInterval(drawElapsedTime, 1000);
 
-    // counter
-    let counter = 0;
-    function drawCount() {
-        counter++;
-        $(score).text(counter + " Hits");  
-    }
+    moleImage.addEventListener("click", (event) => {
+      if (event.target.classList.contains("alive")) {
+        let counterText = document.getElementById("pointsCounter");
 
-    // Auswählen von Tunnel
-    let hole;
-    let oldhole;
-    function chooseHole() {
-        if(time < 0) {
-            clearInterval(holeID);
-        } else {
-            hole = (Math.round(Math.random() * 100)) % amount; 
-            if(hole == oldhole && Math.random() > 0.35) { // weighted randomness: in 65% der Fälle wird neue Zahl ausgewählt, falls das alte Hole, dem neuen entspricht
-                hole = (hole + 1) % amount;
-            }
-            oldhole = hole
-            drawMole(hole); // Funktion drawMole aufrufen
+        event.target.classList.toggle("alive");
+        if (event.target.classList.contains("normalMole")) {
+          settings.counter++;
+        } else if (event.target.classList.contains("yellowMole")) {
+          settings.counter += 2;
+        } else if (event.target.classList.contains("redMole")) {
+          settings.counter--;
         }
-    }
-
-    // let the mole appear/disappear
-    let currentMoleX;
-    let currentMoleY;
-    let molePos; // Array in dem Molepositions gespeichert werden (3 dimensional)
-
-    function drawMole(number) {
-        mHeight = mWidth * (sprite.height / sprite.width);
-
-        // Herausfinden, in welcher Reihe und welche Nummer der Maulwurf hat (wenn z.b. 6 Maulwürfe und 2 Reihen, dann 3 pro Reihe)
-        let whichRow = Math.floor(number/(amount/rows));
-        let rowNumber = number % (amount/rows);
-
-        currentMoleX = startPosLeft+abstand + rowNumber * (mWidth+abstand);
-        currentMoleY = startHeight + whichRow * rowDifference;
-
-        let lowererdMole = currentMoleY+100;
-        let up = true;
-
-        // wie sich Mole bewegt
-        moveID = setInterval(moveMole, 20);
-        function moveMole() {
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            clearInterval();
-            drawTunnel();
-
-            if(lowererdMole > currentMoleY && up) {
-                context.drawImage(sprite, currentMoleX, lowererdMole-=5, mWidth, mHeight)
-            } else if (lowererdMole <= currentMoleY+100) {
-                up = false;
-                context.drawImage(sprite, currentMoleX, lowererdMole+=5, mWidth, mHeight)
-            } else if (lowererdMole > (currentMoleY+100) && !up) {
-                clearInterval(moveID);
-            }
-
-            // set mole posititon
-            molePos = [
-                [   // top right corner [x, y]
-                    [currentMoleX+(mWidth*0.44)/2, lowererdMole + mHeight * 0.17], 
-                    [currentMoleX+(mWidth*0.24)/2, lowererdMole + mHeight * 0.17 + mHeight * 0.33], 
-                    [currentMoleX+(mWidth*0.08)/2, lowererdMole + mHeight * 0.17 + mHeight * 0.66]
-                ], 
-                [   // bottom left corner [x, y]
-                    [currentMoleX+mWidth - (mWidth*0.44)/2, lowererdMole + mHeight * 0.17 + mHeight * 0.33], 
-                    [currentMoleX+mWidth - (mWidth*0.24)/2, lowererdMole + mHeight * 0.17 + mHeight * 0.66], 
-                    [currentMoleX+mWidth - (mWidth*0.08)/2, lowererdMole + mHeight * 0.17 + mHeight]
-                ] 
-            ];
-            console.log(molePos);
-        }
-
-    }
-
-    // Zeichnet den Tunnel, aber nur wenn Sprite geladen ist
-    function drawTunnel() {
-        if(tunnel.loaded) {
-            for (let index = 0; index < amount; index++) {
-                let whichRow = Math.floor(index/(amount/rows));
-                let rowNumber = index % (amount/rows);
-                let tunnelPosX = startPosLeft + tAbstand + rowNumber * (tWidth+tAbstand);
-                let tunnelPosY = startHeight + whichRow * rowDifference + 150; // + 200 weil tunnel unter mole startet
-
-                // Um Verschiebungen bei Sprites vorzuwirken
-                let positioning = ((tWidth-mWidth)/2)-10 ;
-                if (index%3 == 0) {
-                    tunnelPosX = tunnelPosX + positioning;
-                } else if(index%3 == 2) {
-                    tunnelPosX = tunnelPosX - positioning;
-                }
-
-                tHeight = tWidth * (tunnel.height / tunnel.width);
-                context.drawImage(tunnel, tunnelPosX, tunnelPosY, tWidth, tHeight);
-                // Im Vordergrund
-                context.globalCompositeOperation = 'destination-over';
-            }
-        } else {
-            console.log("Fehler");
-        }
-    }
-    
-    // hit detection
-    // https://lavrton.com/hit-region-detection-for-html5-canvas-and-how-to-listen-to-click-events-on-canvas-shapes-815034d7e9f8/
-    canvas.addEventListener('click', (e) => {
-        // Object
-        const mousePos = {
-            x: e.clientX - canvas.offsetLeft,
-            y: e.clientY - canvas.offsetTop
-        };
-
-        // schauen ob sich die Maus irgendwo in einer der Drei "Zonen" des Sprites befindet, immer mit top left corner und bottom right corner
-        if( // mousePos.x >= top right corner x && mousePos.x <= bottom left corner && mousePos.y >= top right corner && mousePos.y <= bottom left corner 
-            mousePos.x >= molePos[0][0][0] && mousePos.x <= molePos[1][0][0] && mousePos.y >= molePos[0][0][1] && mousePos.y <= molePos[1][0][1]
-            || mousePos.x >= molePos[0][1][0] && mousePos.x <= molePos[1][1][0] && mousePos.y >= molePos[0][1][1] && mousePos.y <= molePos[1][1][1]
-            || mousePos.x >= molePos[0][2][0] && mousePos.x <= molePos[1][2][0] && mousePos.y >= molePos[0][2][1] && mousePos.y <= molePos[1][2][1]
-        ) {
-            console.log(mousePos.x >= molePos[0][0][0] && mousePos.x <= molePos[1][0][0] && mousePos.y >= molePos[0][0][1] && mousePos.y <= molePos[1][0][1]);
-            console.log(mousePos.x >= molePos[0][1][0] && mousePos.x <= molePos[1][1][0] && mousePos.y >= molePos[0][1][1] && mousePos.y <= molePos[1][1][1]);
-            console.log(mousePos.x >= molePos[0][2][0] && mousePos.x <= molePos[1][2][0] && mousePos.y >= molePos[0][2][1] && mousePos.y <= molePos[1][2][1]);
-            console.log(molePos[0][2][0] + " " + molePos[1][2][0] + " " + molePos[0][2][1] + " " + molePos[1][2][1])
-            console.log("In it");
-            //setToZero();
-            setToZero();
-            drawCount();
-            clearInterval(moveID);
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            
-            drawTunnel();
-            
-        } 
-        console.log('canvas click' + mousePos.x + " " + mousePos.y);
+        // Updaten der neuen Punktanzahl
+        $(counterText).text("Points: " + settings.counter);
+      }
     });
 
-    function setToZero() {
-        for(let i=0; i<molePos.length; i++) {
-            for(let j=0; j<molePos[i].length; j++) {
-                for(let k=0; k<molePos[i][j].length; k++) {
-                    molePos[i][j][k] =  0;
-                }
-            }
-        }
+    mole.appendChild(imageDirt);
+    // Image an div hängen
+    mole.appendChild(moleImage);
+    // div an game-bereich hängen
+    settings.game.appendChild(mole);
+  }
+
+  // senden der daten am ende
+  $("#endButton").click(function () {
+    console.log(settings.counter);
+    let thishere = document.getElementById("pointsCounter");
+    // Daten senden an
+    $.ajax({
+      url: '/webdev-project-haudenmaulwurf/scoreboard',
+      type: 'POST',
+      dataType: "json",
+      data: {'action':'saveScore', 'score': settings.counter}
+    }).done(function() {
+      console.log("heya");
+      window.location.href = "#"; // "./index.html"
+    });
+
+  });
+
+  // auswählen welche farbe der maulwurf haben soll
+  function colorSetter(moleImage) {
+    let alreadySet =
+      moleImage.classList.contains("normalMole") ||
+      moleImage.classList.contains("yellowMole") ||
+      moleImage.classList.contains("redMole");
+
+    if (settings.difficulty == 1 && !alreadySet) {
+      if (Math.random() < 0.3) {
+        moleImage.classList.add("redMole");
+      } else {
+        moleImage.classList.add("normalMole");
+      }
+    } else if (settings.difficulty == 2 && !alreadySet) {
+      if (Math.random() < 0.1) {
+        moleImage.classList.add("yellowMole");
+      } else if (Math.random() < 0.2) {
+        moleImage.classList.add("redMole");
+      } else {
+        moleImage.classList.add("normalMole");
+      }
+    } else if (settings.difficulty == 3 && !alreadySet) {
+      if (Math.random() < 0.3) {
+        moleImage.classList.add("yellowMole");
+      } else if (Math.random() < 0.2) {
+        moleImage.classList.add("redMole");
+      } else {
+        moleImage.classList.add("normalMole");
+      }
     }
-
-    // gets loaded when the window is loaded
-    function init() {
-        timeID = setInterval(drawElapsedTime, 1000); // 1000 = 1 Sekunde
-        holeID = setInterval(chooseHole, 1500); // defines how often a new hole is chosen 1500 = 1.5 Sekunden
-        drawTunnel();
-    }
-
-    window.onload = init();
-
-});
-
-
-
+  }
+}
--- includes/controllers/GameController.php
+++ includes/controllers/GameController.php
@@ -5,13 +5,12 @@ class GameController extends Controller
     protected $viewFileName = "scoreboard"; //this will be the View that gets the data...
     protected $loginRequired = true;
 
-
     public function run()
     {
         $this->view->title = 'scoreboard';
         $this->view->username = $this->user->username;
 
-        //$this->checkForSaveScorePost();
+        $this->checkForSaveScorePost();
         $this->view->userscores = GameModel::getScoreForOneUserById($this->user->id);
 
         $this->view->scores = GameModel::getTotalScores();
@@ -19,14 +18,12 @@ class GameController extends Controller
 
     private function checkForSaveScorePost()
     {
-        if(isset($_POST['action']) && $_POST['action'] == 'saveScore')
-        {
+        if(isset($_POST['action']) && $_POST['action'] == 'saveScore') {
             $score = $_POST['score'];
-            $userid = $this->user->id;
-            $tmsp = $_POST['tmsp'];
+            $userid = 2;
 
 
-            GameModel::createAndSaveScore($userid, $score, $tmsp);
+            GameModel::createAndSaveScore($userid, $score);
 
 
             $jsonResponse = new JSON();
@@ -34,6 +31,7 @@ class GameController extends Controller
             $jsonResponse->setMessage("Saved the score!");
             $jsonResponse->send();
         }
+
     }
 
 

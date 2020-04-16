d09cfc990811b55084db5cee056e74f6785cb418
Mirjam <klammsteinermirjam@gmail.com>
2020-04-16 09:37:02 +0200

    CSS Error behoben

--- includes/views/game.php
+++ includes/views/game.php
@@ -3,30 +3,29 @@
 echo $this->header;
 
 ?>
+<div class="innerBody">
+    <div id="additionalItems">
+        <div id="pointsCounter">Punkte:</div>
+        <div id="timer">Zeit</div>
+    </div>
+    <div id="game" class="noshow"></div>
 
-<div id="additionalItems">
-    <div id="pointsCounter">Punkte:</div>
-    <div id="timer">Zeit</div>
+    <div id="buttons">
+        <button class="easyButton difficultyButton" value="1">Easy</button>
+        <button class="mediumButton difficultyButton" value="2">Medium</button>
+        <button class="hardButton difficultyButton" value="3">Hard</button>
+        <button class="impossibleButton difficultyButton" value="999">Impossible</button>
+    </div>
+    <div id="endArea">
+        <button id="endButton">Zeige Highscores</button>
+    </div>
+    <div class="audioControllers">
+        <button class="muteButton"><img src="./images/unmutedButton.png" id="mute"></i></button>
+        <button class="volumeButton volumeDown">--</i></button>
+        <button class="volumeButton volumeUp">++</i></button>
+    </div>
 </div>
-<div id="game" class="noshow"></div>
-
-<div id="buttons">
-    <button class="easyButton difficultyButton" value="1">Easy</button>
-    <button class="mediumButton difficultyButton" value="2">Medium</button>
-    <button class="hardButton difficultyButton" value="3">Hard</button>
-    <button class="impossibleButton difficultyButton" value="999">Impossible</button>
-</div>
-<div id="endArea">
-    <button id="endButton">Zeige Highscores</button>
-</div>
-<div class="audioControllers">
-    <button class="muteButton"><img src="./images/unmutedButton.png" id="mute"></i></button>
-    <button class="volumeButton volumeDown">--</i></button>
-    <button class="volumeButton volumeUp">++</i></button>
-</div>
-
 <script type="text/javascript" src="js/game.js"></script>
-
 <?php
 
 echo $this->footer;

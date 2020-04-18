89f63fdae9aa3452485ce47ec2ba48045d98b5df
Mirjam <klammsteinermirjam@gmail.com>
2020-04-18 16:52:07 +0200

    DBconfig change DB name

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', '/webdev-project-haudenmaulwurf');
+define('DB_NAME', 'webdev-project-haudenmaulwurf');
 define('DB_USER', 'Testuser');
 define('DB_PASS', '44YTnNvcHFgLF2M5');

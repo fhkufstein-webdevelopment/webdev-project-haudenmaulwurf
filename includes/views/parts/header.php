f06fbe32295d3c81bb120316927bc978f4ab59d0
Mirjam <klammsteinermirjam@gmail.com>
2020-04-18 16:12:45 +0200

    feedback.css gelöscht und header geändert

--- includes/views/parts/header.php
+++ includes/views/parts/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-	<title><?php echo $this->title; ?></title>
+	<title>Hau den Maulwurf - <?php echo $this->title; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
